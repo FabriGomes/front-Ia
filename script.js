0166b29907faea779b089aecc78c982d1cead0e5
FabriGomes <fabricio.gomesb@outlook.com>
2024-04-03 06:23:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function logar(){
+var login = document.getElementById('usuario').value;
+var senha = document.getElementById('senha').value;
+if(login == "admin" && senha == "admin"){
+    alert('Sucesso');
+    location.href = "home.html";
+} else{
+        alert('Falha');
+    }
+}
